683aad46ddea4e6d38a864adba271399a4a5e340
wxinix <xinwuping007@gmail.com>
2020-05-22 11:54:54 -0400

    Update FSharp.Interop.ComProvider SubModule

--- examples/OpenNetwork/OpenNetwork.fs
+++ examples/OpenNetwork/OpenNetwork.fs
@@ -6,4 +6,9 @@ type Vissim = COM.``Vissim Object Library 20.0 64 Bit``.``14.0-win64``
 let main argv =
     let vissim = Vissim.VissimClass()
     vissim.LoadNet @"C:\Users\Public\Documents\PTV Vision\PTV Vissim 2020\Examples Demo\Urban Intersection Beijing.CN\Intersection Beijing.inpx"
+    let mutable sec = vissim.Simulation.AttValue("SimSec") |> string
+    for i in 1..10 do 
+        vissim.Simulation.RunSingleStep()
+        sec <- vissim.Simulation.AttValue("SimSec") |> string
+        printfn "Current simulation second is %s" sec
     0
--- examples/OpenNetwork/OpenNetworkScript.fsx
+++ examples/OpenNetwork/OpenNetworkScript.fsx
@@ -5,4 +5,3 @@ type Vissim = COM.``Vissim Object Library 20.0 64 Bit``.``14.0-win64``
 
 let vissim = Vissim.VissimClass()
 vissim.LoadNet @"C:\Users\Public\Documents\PTV Vision\PTV Vissim 2020\Examples Demo\Urban Intersection Beijing.CN\Intersection Beijing.inpx"
-
