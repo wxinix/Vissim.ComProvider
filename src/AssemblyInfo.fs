f2ea123cfd7a8d4b58a5e29ec4a58c6a5e70223f
wxinix <xinwuping007@gmail.com>
2020-05-23 22:11:28 -0400

    Update AssemblyInfo

--- src/AssemblyInfo.fs
+++ src/AssemblyInfo.fs
@@ -1,9 +1,9 @@
 ﻿namespace System
 open System.Reflection
 
-[<assembly: AssemblyTitleAttribute("Vissim.ComProvider")>]
+[<assembly: AssemblyTitleAttribute("F# Type Provider for Vissim COM App")>]
 [<assembly: AssemblyProductAttribute("Vissim.ComProvider")>]
-[<assembly: AssemblyDescriptionAttribute("F# type provider for Vissim COM interop.")>]
+[<assembly: AssemblyDescriptionAttribute("")>]
 [<assembly: AssemblyVersionAttribute("1.0.0")>]
 [<assembly: AssemblyCopyrightAttribute("Copyright © Wuping Xin 2020")>]
 [<assembly: AssemblyInformationalVersionAttribute("1.0.0")>]
