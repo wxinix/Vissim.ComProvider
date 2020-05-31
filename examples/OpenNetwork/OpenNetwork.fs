49d44f40e56df178a697650fe396eb36a2a9a058
wxinix <xinwuping007@gmail.com>
2020-05-31 19:32:30 -0400

    Update OpenNetwork example

--- examples/OpenNetwork/OpenNetwork.fs
+++ examples/OpenNetwork/OpenNetwork.fs
@@ -5,27 +5,20 @@ open System.Runtime.InteropServices
 // part of the compiler type system. There is no need to explicitly import the type library
 // or add references.
 
-type VissimLib_540 = COM.``VISSIM_COMServer 5.40 Type Library``.``1.0-win64`` // Alias of Vissim version 540 COM Type Lib
-type VissimLib_100 = COM.``Vissim Object Library 10.0``.``a.0-win32``         // Alias of Vissim version 100 COM Type Lib
-type VissimLib_200 = COM.``Vissim Object Library 20.0 64 Bit``.``14.0-win64`` // Alias of Vissim version 200 COM Type Lib
+// Alias to Vissim 2020 COM Type Lib
+type VissimLib = COM.``Vissim Object Library 20.0 64 Bit``.``14.0-win64``
 
-// The installed Vissim Type Libs are part of the compiler type system. We just alias them with short names.
+[<Literal>]
+let VissimDemoFolder = @"C:\Users\Public\Documents\PTV Vision\PTV Vissim 2020\Examples Demo\"
 
 [<EntryPoint; STAThread>]
 let main argv =
-    let vissim540 = VissimLib_540.VissimClass() // Create a Vissim 540 COM Object instance
-    let vissim100 = VissimLib_100.VissimClass() // Create a Vissim 100 COM Object instance
-    let vissim200 = VissimLib_200.VissimClass() // Create a Vissim 200 COM Object instance
+    // Bind Vissim Demo folder and network file
+    let networkFile = VissimDemoFolder + @"Urban Intersection Beijing.CN\Intersection Beijing.inpx"
 
-    // We can have different versions of Vissim COM objects co-exisit in the same app domain.
-
-    vissim200.LoadNet @"X:\Temp\Urban Intersection Beijing.CN\Intersection Beijing.inpx"
-    for linkObj in vissim200.Net.Links do  
-        let link = linkObj :?> VissimLib_200.ILink // Downcast, ILink is a subtype under VissimLib_200
-        link.AttValue("No") |> string |> printfn "Link No %s"
-
-    let mutable sec = vissim200.Simulation.AttValue("SimSec") |> string
-    printfn "Current simulation second is %s" sec
-    Console.ReadLine() |> ignore
+    let vissim = VissimLib.VissimClass()
+    vissim.LoadNet networkFile
+    // Do nothing and exit.
+    vissim.Exit()
     0
      
--- examples/OpenNetwork/OpenNetworkScript.fsx
+++ examples/OpenNetwork/OpenNetworkScript.fsx
@@ -1,11 +1,20 @@
-﻿// This script can be directly run from F# interactive. Select all the code and
+﻿// This script can be directly run from F# interactive or VS Code. Select all the code and
 // press Alt + Enter to run the script.
 
 #I "../../output"
 #r "Vissim.ComProvider.dll"
 
-type Vissim = COM.``Vissim Object Library 20.0 64 Bit``.``14.0-win64``
+open System.IO
 
-let vissim = Vissim.VissimClass()
-vissim.LoadNet @"C:\Users\Public\Documents\PTV Vision\PTV Vissim 2020\Examples Demo\Urban Intersection Beijing.CN\\
-                 Intersection Beijing.inpx"
+// Alias to Vissim 2020 COM Type Lib
+type VissimLib = COM.``Vissim Object Library 20.0 64 Bit``.``14.0-win64``
+
+// Bind Vissim Demo folder and network file
+[<Literal>]
+let VissimDemoFolder = @"C:\Users\Public\Documents\PTV Vision\PTV Vissim 2020\Examples Demo"
+let networkFile = VissimDemoFolder + @"\Urban Intersection Beijing.CN\Intersection Beijing.inpx"
+
+let vissim = VissimLib.VissimClass()
+vissim.LoadNet networkFile
+// Do nothing and exit.
+vissim.Exit()
