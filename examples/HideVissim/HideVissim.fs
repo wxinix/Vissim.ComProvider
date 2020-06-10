caf08c12957bb6b1d94fec385a624e344676d860
wxinix <xinwuping007@gmail.com>
2020-06-10 14:50:27 -0400

    Update HideVissim example

--- examples/HideVissim/HideVissim.fs
+++ examples/HideVissim/HideVissim.fs
@@ -20,6 +20,7 @@
 // SOFTWARE.
 
 open System
+open System.Runtime.InteropServices
 
 // Vissim COM Type Provider collects all installed Vissim COM type libraries and make them
 // part of the compiler type system. There is no need to explicitly import the type library
@@ -29,9 +30,64 @@ open System
 type VissimLib =
      Vissim.ComProvider.``Vissim Object Library 20.0 64 Bit``.``14.0-win64``
 
+[<DllImport("Vissim.ComProvider.Utilities.dll", CallingConvention = CallingConvention.StdCall)>]
+extern void HideVissim(nativeint unk);
+
+[<DllImport("Vissim.ComProvider.Utilities.dll", CallingConvention = CallingConvention.StdCall)>]
+extern void ShowVissim(nativeint unk);
+
+let [<Literal>] ExampleFolder = @"C:\Users\Public\Documents\PTV Vision\PTV Vissim 2020\Examples Training\COM\"
+let [<Literal>] LayoutFile    = ExampleFolder + @"Basic Commands\COM Basic Commands.layx"
+let [<Literal>] NetworkFile   = ExampleFolder + @"Basic Commands\COM Basic Commands.inpx"
+
+type VissimLib.IVissim with
+    member this.HideMainWindow () =
+        printfn "Inside HideMainWindow"
+        let unk = Marshal.GetIUnknownForObject this
+        HideVissim(unk)
+        Marshal.Release(unk)
+    
+    member this.RestoreMainWindow () =
+        let unk = Marshal.GetIUnknownForObject this
+        ShowVissim(unk)
+        Marshal.Release(unk)
+
 [<EntryPoint; STAThread>]
 let main argv =
+    let vissim = VissimLib.VissimClass()
+    let simPeriod = vissim.Simulation.AttValue("SimPeriod") :?> int
+    let stopWatch = new System.Diagnostics.Stopwatch ()
+    
+    vissim.LoadNet NetworkFile
+    
+    let runVissimWithoutGUI =
+        printfn "Running Vissim  with hidden Main Window now..."        
+        vissim.HideMainWindow |> ignore 
+        stopWatch.Restart()
+        vissim.Simulation.RunContinuous()
+        stopWatch.Stop()
+        let time = stopWatch.ElapsedMilliseconds
+        vissim.RestoreMainWindow |> ignore
+        let realtimeFactor = (double simPeriod * 1000.0) / double time 
+    
+        (time, simPeriod, realtimeFactor) 
+        |||> printfn "With hidden Vissim, it taks %d ms to run %d sec simulation, achieved realtime factor %f" 
+    
 
+    let runVissimWithGUI =
+        printfn "Running Vissim with normal Main Window now..."
+        stopWatch.Restart()
+        vissim.Simulation.RunContinuous()
+        stopWatch.Stop()
+        let time = stopWatch.ElapsedMilliseconds
+
+        let realtimeFactor = (double simPeriod * 1000.0) / double time 
+    
+        (time, simPeriod, realtimeFactor) 
+        |||> printfn "With normal Vissim, it taks %d ms to run %d sec simulation, achieved realtime factor %f" 
+    
+    runVissimWithoutGUI
+    runVissimWithGUI
     Console.ReadLine() |> ignore
     0
 
