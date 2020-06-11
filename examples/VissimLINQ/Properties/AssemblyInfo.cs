1e9a4f2d87f893debb75e570f47a11638d218354
wxinix <xinwuping007@gmail.com>
2020-06-10 23:14:22 -0400

    Update VissimLINQ example

--- examples/VissimLINQ/Properties/AssemblyInfo.cs
+++ examples/VissimLINQ/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CSharpTest")]
+[assembly: AssemblyTitle("Illustrating Vissim COM with LINQ")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CSharpTest")]
+[assembly: AssemblyProduct("VissimLINQ")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
